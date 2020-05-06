96c5a9f51fd5c82b50e4ab432d2f5a3aff7f1392
ChongLynk <tronglinhmta0611@gmail.com>
2020-05-06 05:05:24 -0400

    fix conflic

--- dog.js
+++ dog.js
@@ -3,6 +3,7 @@ function dog(){
 }
 dog.prototype.eat = function(cat){
     this.tomatch.push(cat);
-}dog.prototype.jump = function(){
+}
+dog.prototype.jump = function(){
     console.log("jumping........");
 }
