9e4da908c3158c866efad373bf751d50c4b7111e
ChongLynk <tronglinhmta0611@gmail.com>
2020-05-06 09:49:38 -0400

    add method run for mouse

--- mouse.js
+++ mouse.js
@@ -2,6 +2,11 @@ function mouse(name, weigth){
     this.name = name;
     this.weigth= weigth;
 }
+
 mouse.prototype.jump = function(){
     console.log("jumping.............!!!");
+}
+mouse.prototype.run = function(){
+    console.log("running.........!!!");
+
 }
